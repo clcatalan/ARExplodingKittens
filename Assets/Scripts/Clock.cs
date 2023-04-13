6881c31b0e79b1546d22df292e23e3067ef20323
lstubbs8 <lstubbs8@gatech.edu>
2023-04-13 00:21:01 -0400

    Time Up Animations
    Fixed Clock mesh and material. Added animation, text and script to Clock Prefab.

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro txt;
+    [SerializeField] private MeshRenderer txtEnable;
+    [SerializeField] private GameObject clock;
+    private Animator animController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animController = GetComponent<Animator>();
+        StopTimeUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartTimeUp();
+        } else if (Input.GetKeyDown(KeyCode.V))
+        {
+            StopTimeUp();
+        }
+    }
+
+    void StopTimeUp()
+    {
+        txtEnable.enabled = false;
+        clock.SetActive(false);
+        animController.SetBool("Time Up", false);
+    }
+
+    void StartTimeUp()
+    {
+        txtEnable.enabled = true;
+        clock.SetActive(true);
+        animController.SetBool("Time Up", true);
+    }
+}
